ae71f4ffca01ab76b8e5bed0f0417436b6b09733
ord669 <ord24@gmail.com>
2023-02-25 00:15:15 +0700

    added session with google auth

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,8 +1,11 @@
+'use client'
+import { useSession } from "next-auth/react"
 import NewChat from "./NewChat"
 
 function SideBar() {
+    const { data: session } = useSession()
     return (
-        <div className="p-2 flex-col h-screen">
+        <div className="p-2 flex flex-col h-screen">
             <div className="flex-1">
                 <div>
                     {/* New Chat */}
@@ -16,6 +19,12 @@ function SideBar() {
 
                 </div>
             </div>
+
+            {session &&
+                <img src={session.user?.image || ''} alt="Profile picture"
+                    className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+                />
+            }
         </div>
     )
 }
--- next.config.js
+++ next.config.js
@@ -4,4 +4,7 @@ module.exports = {
     appDir: true,
   },
   reactStrictMode: true,
+  images: {
+    domains: ['links.papareact.com']
+  },
 }
